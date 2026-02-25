19a54649a610fb70f43ee5464050b03ec586f371
Bhumika-0921 <vadherbhoomika@gmail.com>
2026-02-25 11:45:32 +0530

    inital project setup

---
+++ modulsComentPip.py
@@ -0,0 +1,9 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# pip is the package manager for pyton ,it used to install any moules in python\
+
+# two types of mosul one 1)builtin lke numpy  and 2)extranal module lke tenserflow,flask etc..
+
